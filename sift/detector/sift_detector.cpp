3dc362619e411d483d36bd6ac6bd7d9b9ced60a3
biotrump <thomas@biotrump.com>
2014-02-23 22:57:30 +0800

    openCV 3.x init

--- sift/detector/sift_detector.cpp
+++ sift/detector/sift_detector.cpp
@@ -56,11 +56,13 @@ cv2.imwrite('sift_keypoints.jpg',img)
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui_c.h>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/legacy/legacy.hpp>
 
 using namespace cv;
+using namespace std;
 
 static void help( char** argv )
 {
